3a0c9622bd1392fbee9d78f403329b3748aecf97
--replace-all <belaashu23@gmail.com>
2023-07-16 15:49:43 +0200

    Simple C++ program to strength my C++ skills

---
+++ GitRecap.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main () {
+    std::cout << "Yoww bella, it's me again!" << std::endl; 
+    std::cout << "You are getttng better by the day" << std::endl; 
+
+    return 0; 
+}
