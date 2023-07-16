7c28eaa83bf2342b8483260bb8ffd603e6a7b143
--replace-all <belaashu23@gmail.com>
2023-07-16 15:58:30 +0200

    Added a 3rd line

--- GitRecap.cpp
+++ GitRecap.cpp
@@ -3,6 +3,7 @@
 int main () {
     std::cout << "Yoww bella, it's me again!" << std::endl; 
     std::cout << "You are getttng better by the day" << std::endl; 
+    std::cout << "Remember resilience, sacrifice, and hardwork paysoff" << std::endl; 
 
     return 0; 
 }
